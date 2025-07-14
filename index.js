776441b2a9162afbe4a6b43799b318bb1ed3ba12
Jeffrey Chang <jeffreyc@kth.se>
2025-07-14 16:35:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,24 @@ const twitterClient = client.readWrite;
 // Load quotes from quotes.json
 const quotes = JSON.parse(fs.readFileSync('./quotes.json', 'utf8'));
 
+const generateCatTranslation = (englishText) => {
+  const catSounds = ['meow', 'purr', 'hiss', 'mrow', 'mew', 'chirp', 'ss', 'yowl'];
+  const words = englishText.toLowerCase().replace(/[^a-z\s]/g, '').split(' ');
+  let catTranslation = '';
+  for (let i = 0; i < words.length; i++) {
+    catTranslation += catSounds[Math.floor(Math.random() * catSounds.length)];
+    if (i < words.length - 1) {
+      catTranslation += ' ';
+    }
+  }
+  return catTranslation.charAt(0).toUpperCase() + catTranslation.slice(1) + '.';
+};
+
 const tweetQuote = async () => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const { english, cat_translation } = quotes[randomIndex];
 
-  const tweetText = `"${english}"
-
-Cat Translation:
-${cat_translation}`;
+  const tweetText = `"${english}"\n\nCat Translation:\n${cat_translation}`;
 
   try {
     const truncatedTweet = tweetText.length > 280 ? tweetText.substring(0, 277) + '...' : tweetText;
@@ -34,7 +44,6 @@ ${cat_translation}`;
     console.error('Error tweeting:', error);
   }
 };
-
 
 cron.schedule('0 */4 * * *', () => {
   console.log('Running the twitter bot...');
